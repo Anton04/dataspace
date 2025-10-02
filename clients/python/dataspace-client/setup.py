a88d4b4198261c9f00f51ad5a859d90fde1a15b5
Anton04 <anton.gustafsson@ri.se>
2025-10-02 09:26:14 +0200

    Update setup.py
    added extra for certain requirements that might interact with certain enviroments.

--- clients/python/dataspace-client/setup.py
+++ clients/python/dataspace-client/setup.py
@@ -2,21 +2,35 @@ from setuptools import setup, find_packages
 
 setup(
     name='dataspace-client',
-    version='0.1.3.15',
+    version='0.1.3.16',
     author='Anton Gustafsson',
     author_email='anton.gustafsson@ri.se',
-    description='This is a tool for connecting to a dataspace created with publish subscribe paradigm.',
+    description='Client for a publish/subscribe dataspace (MQTT-based) with convenience helpers.',
     packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[
+        'paho-mqtt>=1.5.0',
+        'pytz',
+        # OBS: inga tunga binärer här
+    ],
+    extras_require={
+        # Installera dataframe-stöd separat
+        'dataframe': [
+            'pandas>=2.2.2,<3.0',
+        ],
+        # 3D/visnings-stöd separat (kan dra in numpy)
+        'vis': [
+            'trimesh>=4.4.9',
+        ],
+        # allt
+        'all': [
+            'pandas>=2.2.2,<3.0',
+            'trimesh>=4.4.9',
+        ],
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-        'paho-mqtt>=1.5.0',  # Add this line to specify paho-mqtt as a dependency
-        'trimesh>=4.4.9',
-        'pandas>=2.2.2',
-        'pytz',
     ],
 )
